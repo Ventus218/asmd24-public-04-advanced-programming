c3c6ded79a32b09945e3215f6cf7cb2da3dbd6a1
mirko.viroli <mirko.viroli@unibo.it>
2025-03-27 15:37:05 +0100

    better generator

--- src/test/scala/lab04/SetADTCheck.scala
+++ src/test/scala/lab04/SetADTCheck.scala
@@ -1,22 +1,37 @@
 package scala.lab04
 
 import org.scalacheck.Prop.forAll
-import org.scalacheck.{Arbitrary, Properties}
+import org.scalacheck.{Arbitrary, Gen, Properties}
 
 import scala.lab04.Sets.{BasicSetADT, SetADT}
 
 abstract class SetADTCheck(name: String) extends Properties(name):
-  val ADT: SetADT
+  val setADT: SetADT
 
-  given Arbitrary[ADT.Set[Int]] = Arbitrary:
+  // generating a small Int
+  def smallInt(): Gen[Int] = Gen.choose(0, 10)
+  // generating a Set of Int with approximate size (modulo clashes)
+  def setGen[A: Arbitrary](size: Int): Gen[setADT.Set[A]] =
+    if size == 0
+      then Gen.const(setADT.empty())
+    else for
+      a <- Arbitrary.arbitrary[A]
+      s <- setGen(size - 1)
+    yield setADT.of(a).union(s)
+  // a given instance to generate sets with small size
+  given arb: Arbitrary[setADT.Set[Int]] = Arbitrary:
     for
-      n <- Arbitrary.arbitrary[Int]
-    yield ADT.of(n)
+      i <- smallInt()
+      s <- setGen[Int](i)
+    yield s
 
+  property("commutativity of union") =
+    forAll: (s1: setADT.Set[Int], s2: setADT.Set[Int]) =>
+      (s1 || s2) === (s2 || s1)
 
-  property("union - commutative") = forAll:
-    (a: ADT.Set[Int], b: ADT.Set[Int]) =>
-      (a || b) === (b || a)
 
 object BasicSetADTCheck extends SetADTCheck("SequenceBased Set"):
-  val ADT: SetADT = BasicSetADT
+  val setADT: SetADT = BasicSetADT
+
+  @main def visuallingCheckArbitrarySets =
+    Range(0,20).foreach(i => println(summon[Arbitrary[setADT.Set[Int]]].arbitrary.sample))
